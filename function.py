24f8b45d1bf540a131e26c53214b05c22e0ff9ce
DHANYAV20 <60929233+DHANYAV20@users.noreply.github.com>
2025-05-01 18:44:51 +0530

    Add files via upload

---
+++ function.py
@@ -0,0 +1,4 @@
+def f(x): # here def is written in snake_case # first line of the defining the function id called signature of the function
+    return 2 * x + 1
+
+#define function to reuse the code 
